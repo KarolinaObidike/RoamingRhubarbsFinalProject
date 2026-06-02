8778b765869b19f8de5f53884294c20a34fee27a
Sam-py31 <samturver1@gmail.com>
2026-06-02 11:51:24 +0100

    updated main.py and moved into ETL folders

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #THIS CODE WILL BE RESPONSIBLE FOR LOADING ALL FUNCTIONS
 from databases import create_database
 from databases import connectdb
+from ETL import Extract
+from ETL import transform
 
 create_database.setup_db()
+
+rawdata = Extract.get_data()
+
+transform.transform(rawdata)
