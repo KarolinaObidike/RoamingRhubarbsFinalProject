acc0b600653fe9de6a30622cb0c24afb2912df3c
Sam-py31 <samturver1@gmail.com>
2026-06-03 15:14:02 +0100

    changed function and file names to match

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # THIS CODE WILL RUN THE FULL ETL PIPELINE
 from databases import create_database, connectdb
-from ETL import Extract, transform
+from ETL import Extract, Transform
 from ETL.load import load_all
 
 
 def main():
-    create_database.setup_db()
+
 
     rawdata = Extract.get_data()
-    transformed_data = transform.transform(rawdata)
+    transformed_data = Transform.transform(rawdata)
 
     with connectdb.get_connection() as conn:
         load_counts = load_all(transformed_data, conn)
